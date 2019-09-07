d9e990efec9ec71bf68b6e9ca34e2825df5274ee
Andrey Marhalyk <aom@anadeainc.com>
2019-09-07 12:21:53 +0300

    Chat id should be exist

--- db/migrate/20190728120651_create_telegram_messages.rb
+++ db/migrate/20190728120651_create_telegram_messages.rb
@@ -2,7 +2,7 @@ class CreateTelegramMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :telegram_messages do |t|
       t.text     :body, null: false
-      t.string   :chat_id
+      t.string   :chat_id, null: false
       t.datetime :send_at
       t.datetime :sent_at
       t.timestamps
