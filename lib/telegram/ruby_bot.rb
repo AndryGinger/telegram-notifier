220dec33f21c1db1595d317e039dc0a3dcc8e39f
Andrey Marhalyk <aom@anadeainc.com>
2019-09-15 11:45:11 +0300

    Fix get attachment method

--- lib/telegram/ruby_bot.rb
+++ lib/telegram/ruby_bot.rb
@@ -9,11 +9,14 @@ module Telegram
     end
 
     def send_message(message)
+      p message
       response = if message.attachment?
         bot.send_photo(chat_id: message.chat_id, photo: File.open(message.attachment.file.file), caption: message.body)
       else
+        p message.body
         bot.send_message(chat_id: message.chat_id, text: message.body)
       end
+      p response
       message.update_attributes(sent_at: Time.current) if response['ok']
     end
 
@@ -26,9 +29,10 @@ module Telegram
 
       telegram_attributes = { chat_message_id: message[:message_id], body: message[:text] }
       if message[:photo]
+        p message[:photo]
         photo = message[:photo].last
         telegram_attributes[:remote_attachment_url] = attachment_url(photo[:file_id])
-        telegram_attributes[:body] = photo[:caption]
+        telegram_attributes[:body] = message[:caption]
       end
 
       TelegramMessage.create!(telegram_attributes)
@@ -37,7 +41,9 @@ module Telegram
     private
 
     def attachment_url(file_id)
-      file_path = HTTPI.get("https://api.telegram.org/bot#{bot.token}/getFile?file_id=#{file_id}")['file_path']
+      p "https://api.telegram.org/bot#{bot.token}/getFile?file_id=#{file_id}"
+      response = HTTPI.get("https://api.telegram.org/bot#{bot.token}/getFile?file_id=#{file_id}")
+      file_path = JSON.parse(response.raw_body)['result']['file_path']
       "https://api.telegram.org/file/bot#{bot.token}/#{file_path}"
     end
   end
