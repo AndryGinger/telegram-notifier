468c9f57923adb36912f771b12979fa99852a5b0
Andrey Marhalyk <aom@anadeainc.com>
2019-09-15 11:03:34 +0300

    Update ready to send query

--- app/models/telegram_message.rb
+++ app/models/telegram_message.rb
@@ -1,5 +1,7 @@
 class TelegramMessage < ApplicationRecord
   mount_uploader :attachment, AttachmentUploader
 
-  scope :ready_to_send, -> { where("send_at <= (?) AND sent_at IS NULL AND chat_id IS NOT NULL", Time.current) }
+  scope :ready_to_send, -> {
+    where("send_at <= (?) AND sent_at IS NULL AND chat_id IS NOT NULL AND
+          (body IS NOT NULL OR attachment IS NOT NULL)", Time.current) }
 end
