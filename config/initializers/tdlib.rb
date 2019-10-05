7e508ae6c3d2e8cc777a0d7619b954f5e5f0f6b7
Andrey Marhalyk <aom@anadeainc.com>
2019-10-05 18:32:56 +0300

    Add download and uploading functionality for photos and documents

--- config/initializers/tdlib.rb
+++ config/initializers/tdlib.rb
@@ -1,6 +1,8 @@
 require "#{Rails.root}/lib/telegram/client.rb"
 require 'tdlib-ruby'
 
+return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 'rake' || ENV["SKIP_SCHEDULER"]
+
 SCHEDULER = Rufus::Scheduler.singleton
 
 TD.configure do |config|
@@ -8,6 +10,9 @@ TD.configure do |config|
 
   config.client.api_id = ENV["API_ID"]
   config.client.api_hash = ENV["API_HASH"]
+  config.client.use_file_database = false
+  config.client.use_chat_info_database = false
+  config.client.use_message_database = false
 end
 
 TD::Api.set_log_verbosity_level(1)
@@ -18,11 +23,13 @@ td_client.on(TD::Types::Update::NewMessage) do |update|
   TELEGRAM_CLI.create_telegram_message(update.message)
 end
 
+td_client.on(TD::Types::Update::File) do |update|
+  TELEGRAM_CLI.upload_downloaded_file(update.file)
+end
+
 td_client.connect
 
 TELEGRAM_CLI = Telegram::Client.new(td_client)
-
-return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 'rake' || ENV["SKIP_SCHEDULER"]
 
 SCHEDULER.every '30s' do |job|
   TelegramMessage.ready_to_send.each do |telegram_message|
--- lib/telegram/client.rb
+++ lib/telegram/client.rb
@@ -7,29 +7,96 @@ module Telegram
     end
 
     def send_message(message)
-      response = @client.send_message(message.chat_id, message_content(message.body)).wait
+      @client.get_chats.wait
+      p response = @client.send_message(message.chat_id, message_content(message)).wait
       message.update_attributes(sent_at: Time.current) if response.result.first
     end
 
     def create_telegram_message(message)
       chat = Chat.find_by(telegram_chat_id: message.chat_id)
-      return unless chat&.subscribed?
+      # TODO comment required for production
+      return unless chat&.subscribed? #&& message.sender_user_id != @client.get_me.value.id
 
-      TelegramMessage.create!(body: message.content.text.text)
+      content = message.content.attributes
+
+      telegram_attributes = { attachment_attributes: {} }.tap do |attrs|
+        if content[:photo]
+          photo = content[:photo].sizes.last
+
+          attrs[:attachment_attributes][:attachment_id] = photo.photo.id
+          attrs[:attachment_attributes][:attachment_type] = :photo
+          attrs[:body] = content[:caption].text
+
+          @client.download_file(photo.photo.id)
+        elsif content[:document]
+          document = content[:document].document
+
+          attrs[:attachment_attributes][:attachment_id] = document.id
+          attrs[:attachment_attributes][:attachment_type] = :document
+          attrs[:body] = content[:caption].text
+
+          @client.download_file(document.id)
+        elsif content[:text]
+          attrs[:body] = content[:text].text
+        end
+      end
+
+      TelegramMessage.create!(telegram_attributes)
+    end
+
+    def upload_downloaded_file(file)
+      local_file = file.local
+      return unless local_file.is_downloading_completed && local_file.can_be_downloaded
+
+      file_to_save = Pathname.new(local_file.path).open
+      Attachment.where(attachment_id: file.id).each do |tm|
+        tm.attachment = file_to_save
+        tm.save
+      end
+
+      File.delete(local_file.path) if File.exist?(local_file.path)
     end
 
     private
 
-    def message_content(content)
-      formatted_text = TD::Types::FormattedText.new(text: content, entities: [])
-      TD::Types::InputMessageContent::Text.new(text: formatted_text,
-                                               disable_web_page_preview: false,
-                                               clear_draft: false)
+    def message_content(message)
+      content = formatted_text(message.body)
+
+      if attachment = message.attachment
+        local_file = TD::Types::InputFile::Local.new(path: attachment.attachment.path)
+
+        if attachment.photo? || attachment.attachment_type.nil? && attachment.is_image?
+          thumb = attachment.attachment.thumb
+          TD::Types::InputMessageContent::Photo.new(
+            photo: local_file,
+            thumbnail: TD::Types::InputThumbnail.new(
+              thumbnail: TD::Types::InputFile::Local.new(path: thumb.path),
+              width: thumb.width,
+              height: thumb.height
+            ),
+            added_sticker_file_ids: [0],
+            width: attachment.width,
+            height: attachment.height,
+            caption: content,
+            ttl: 0
+          )
+        else
+          TD::Types::InputMessageContent::Document.new(document: local_file, caption: content)
+        end
+      else
+        TD::Types::InputMessageContent::Text.new(text: content,
+                                                 disable_web_page_preview: false,
+                                                 clear_draft: false)
+      end
     end
 
     def method_missing(m, *args, &block)
       puts "Delegating #{m} to TD Client"
       @client.send(m, *args, &block)
+    end
+
+    def formatted_text(content)
+      TD::Types::FormattedText.new(text: content, entities: [])
     end
   end
 end
