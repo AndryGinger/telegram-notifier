242ef9ada17e8a747b8b9b0dd894f41de08e91c4
Andrey Marhalyk <aom@anadeainc.com>
2019-10-12 17:16:48 +0300

    Add wakeup scheduler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+  before_action :run_wakeup_scheduller
+
+  def run_wakeup_scheduller
+    return SCHEDULER.jobs.any? { |j| j.original == "20m" && j.scheduled? }
+
+    SCHEDULER.every "20m" do
+      HTTPI.get("https://stark-gorge-40519.herokuapp.com/")
+    end
+  end
 end
